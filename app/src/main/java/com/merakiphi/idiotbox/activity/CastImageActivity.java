0e4a6b544ffc8e9d54d5219c3952f9fbdf508320
anuragMaravi <anuragmaravi12@gmail.com>
2017-04-09 19:34:56 +0530

    Image Viewer for cast.

--- app/src/main/java/com/merakiphi/idiotbox/activity/CastImageActivity.java
+++ app/src/main/java/com/merakiphi/idiotbox/activity/CastImageActivity.java
@@ -66,23 +66,15 @@ public class CastImageActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Some Error Occured", Toast.LENGTH_SHORT).show();
             }
         });
-        // Add the request to the RequestQueue.
         VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequestCastingImages);
-//
-//        CastImageAdapter adapter = new CastImageAdapter(getApplicationContext(), getIntent().getStringExtra("image_path"));
-//        viewPager.setAdapter(adapter);
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == android.R.id.home) {
-            // finish the activity
             onBackPressed();
             return true;
         }
