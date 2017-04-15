8f1a988cd637d8ef13589f5e011d4141f533857c
“anuragMaravi” <anuragmaravi12@gmail.com>
2017-04-16 01:57:21 +0530

    - Changed to BottomNav Bar

--- app/src/main/java/com/merakiphi/idiotbox/activity/MainActivity.java
+++ app/src/main/java/com/merakiphi/idiotbox/activity/MainActivity.java
@@ -76,50 +76,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
 
-        // show menu only when home fragment is selected
-//        if (navItemIndex == 0) {
-//        }
-
-        // when fragment is notifications, load the menu created for notifications
-//        if (navItemIndex == 3) {
-//            getMenuInflater().inflate(R.menu.notifications, menu);
-//        }
         return true;
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        SearchView search = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.search));
-//        // Associate searchable configuration with the SearchView
-//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-//        search.setSearchableInfo(searchManager.getSearchableInfo(new ComponentName(this, SearchResultsActivity.class)));
-//        search.setQueryHint(getResources().getString(R.string.search_hint));
-//
-//        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_logout) {
-//            Toast.makeText(getApplicationContext(), "Logout user!", Toast.LENGTH_LONG).show();
-//            return true;
-//        }
 
         if (id == R.id.search) {
         Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
         startActivity(intent);}
-
-        // user is in notifications fragment
-        // and selected 'Clear All'
-//        if (id == R.id.action_clear_notifications) {
-//            Toast.makeText(getApplicationContext(), "Clear all notifications!", Toast.LENGTH_LONG).show();
-//        }
 
         return super.onOptionsItemSelected(item);
     }
